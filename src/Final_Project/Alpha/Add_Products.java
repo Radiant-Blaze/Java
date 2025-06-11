21b27379f8277a841125062f4735d987b67a2505
Radiant-Blaze <talhaahmed8760@gmail.com>
2025-06-11 18:08:05 +0500

    11/6/25

--- src/Final_Project/Alpha/Add_Products.java
+++ src/Final_Project/Alpha/Add_Products.java
@@ -7,6 +7,15 @@ public class Add_Products extends Display_Parent
     public void Display()
     {
         Start_Lines();
+        System.out.println("Previous Stock: ");
+        Line();
+
+        System.out.println("What will you do today?");
+        Line();
+
+        System.out.println("1. Add a new product");
+        System.out.println("2. Add an existing product");
+        Selection();
     }
 
     @Override
@@ -18,15 +27,6 @@ public class Add_Products extends Display_Parent
     public void Start_Lines()
     {
         System.out.println("---------------Welcome to Add Product Section---------------\n");
-        System.out.println("Previous Stock: ");
-        Line();
-
-        System.out.println("What will you do today?");
-        Line();
-
-        System.out.println("1. Add a new product");
-        System.out.println("2. Add an existing product");
-        Selection();
     }
 
     public static void main(String[] args)
